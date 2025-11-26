38338e6b36c039bb53db23c58297c38304511335
Stan <s.andujar@proton.me>
2025-11-26 23:48:48 +0100

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import pluginConfig from '@elizaos/config/eslint/eslint.config.plugin.js';
 /**
  * ESLint configuration for plugin-telegram
  * Extends the standard ElizaOS plugin configuration which includes:
- * - @elizaos/structured-logging rule (enforces LOGGING_SPEC.md)
+ * - @elizaos/structured-logging rule
  * - TypeScript support
  * - Standard code quality rules
  */
